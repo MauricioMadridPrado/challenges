b3f1048e229e6f548ac48b4b0c4340e666d984d3
Mauricio Madrid Prado <mauriciomadridprado@gmail.com>
2023-06-08 12:55:17 -0300

    fixed: dog api random img more smoothly

--- pages/API's/dog-api/js/animImg.js
+++ pages/API's/dog-api/js/animImg.js
@@ -1,8 +1,8 @@
 export default function animImg() {
   const image = document.querySelector('[data-img="img"]');
   const imgSlide = [
-    { transform: `translateX(150px)` },
-    { transform: `translateX(0px)` },
+    { transform: `translateX(150px)`,opacity :0 },
+    { transform: `translateX(0px)`,opacity :1 },
   ];
 
   const imgTime = {
